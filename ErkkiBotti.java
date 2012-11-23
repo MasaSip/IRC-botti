0cfc0db39981b6946c910043bf94d02c7ae38588
Masa <matti.sippola@aalto.fi>
2012-11-23 19:36:42 +0200

    pien lisäys edelliseen

--- ErkkiBotti.java
+++ ErkkiBotti.java
@@ -15,9 +15,11 @@ import org.jibble.pircbot.*;
 
 
 public class ErkkiBotti extends PircBot {
-    public static String server = "irc.cs.hut.fi";
-	public static String kanava = "!pahaolo";
-    public static String login = "ErkkiHiippari43";
+    public static String server = "irc.freenode.net";
+    //public static String server = "irc.cs.hut.fi";
+	//public static String kanava = "!pahaolo";
+    public static String kanava = "#pahaolo";
+	public static String login = "ErkkiHiippari43";
     public static String nick = "ErkkixD1";
    
 	Set<BottiModuuli> moduulit;
